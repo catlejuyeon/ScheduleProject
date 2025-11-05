a704d0414b66660baa218e445e8ea8ee77d11a75
castlejuyeon <jucess1106@naver.com>
2025-11-05 20:03:49 +0900

    Feat: 전역 예외 처리용 @ExceptionHandler 구현

---
+++ src/main/java/com/example/scheduleproject/exception/ErrorResponse.java
@@ -0,0 +1,22 @@
+package com.example.scheduleproject.exception;
+
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+@Getter
+public class ErrorResponse {
+    private int status;
+    private String message;
+    private Map<String, String> errors;
+    private LocalDateTime timestamp;
+
+    public ErrorResponse(int value, String message, Map<String, String> errors, LocalDateTime timestamp) {
+        this.status = value;
+        this.message = message;
+        this.errors = errors;
+        this.timestamp = timestamp;
+
+    }
+}
---
+++ src/main/java/com/example/scheduleproject/exception/GlobalExceptionHandler.java
@@ -0,0 +1,72 @@
+package com.example.scheduleproject.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    //1. 400
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleValidationException(MethodArgumentNotValidException e) {
+        Map<String, String>errors=new HashMap<>();
+        e.getBindingResult().getAllErrors().forEach(error->{
+            String fieldName= ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName,errorMessage);
+        });
+
+        ErrorResponse errorResponse=new ErrorResponse(
+                HttpStatus.BAD_REQUEST.value(),
+                "입력값이 올바르지 않습니다.",
+                errors,
+                LocalDateTime.now()
+        );
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+    }
+
+    //2. 401
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ErrorResponse> handleIllegalArgumentException(
+            IllegalArgumentException e) {
+        ErrorResponse errorResponse=new ErrorResponse(
+                HttpStatus.UNAUTHORIZED.value(),
+                e.getMessage(),
+                null,
+                LocalDateTime.now()
+        );
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(errorResponse);
+    }
+
+    //3. 404
+    @ExceptionHandler(IllegalStateException.class)
+    public ResponseEntity<ErrorResponse> handleIllegalStateException(IllegalStateException e) {
+        ErrorResponse errorResponse=new ErrorResponse(
+                HttpStatus.NOT_FOUND.value(),
+                e.getMessage(),
+                null,
+                LocalDateTime.now()
+        );
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+    }
+
+    //4. 500(그외 모든 예외)
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleException(Exception e) {
+        ErrorResponse errorResponse=new ErrorResponse(
+                HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                "서버 내부 오류입니다.",
+                null,
+                LocalDateTime.now()
+        );
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
+    }
+}
